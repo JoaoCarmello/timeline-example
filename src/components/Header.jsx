35ff4bc848f092303313efeca8156a9f8a470f4e
unknown <Carmello@SOLCTI-110.solfarma.com.br>
2025-08-26 23:09:23 -0300

    Timeline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
 import styles from './Header.module.css'
-import igniteLogo from '../assets/Ignite-logo.svg'
 
 export function Header() {
     return (
-        <header className={styles.header}>
-            <img src={igniteLogo} alt="Ignite Logo" />
-        </header>
+        <header className={styles.header}/>
     )
 }
